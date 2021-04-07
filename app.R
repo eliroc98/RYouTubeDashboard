e305108b7bb9bf07592ff859e6fa74c09aac6793
Elisabetta Rocchetti <elisabetta.rocchetti0@gmail.com>
2021-04-07 19:20:37 +0200

    plots

--- app.R
+++ app.R
@@ -1,17 +1,28 @@
+setwd("C:/Users/lizzy/Desktop/Universita/coding for data science and data management/R/project/YouTubeChannels")
 library(shiny)
 library('tidyverse')
+library(DT)
+library(bslib)
+source("r_files\\utils.R")
 
 #import data: data acquisition is performed once because of the YouTube API limit quota
 data <- read.csv(file = 'data/dataset_adjusted.csv')
 
 max_subs <- max(data$statistics_subscriberCount, na.rm=T)
+d<-data[1:10,]
+df<-data.frame(channel<-rep(d$snippet_title,2), count=c(d$statistics_subscriberCount,d$statistics_viewCount),count_type=c(rep("subscriberCount",nrow(d)),rep("viewCount",nrow(d))))
+ggplot(df, aes(x = channel,fill=count_type,y=count)) +
+    geom_col(position = position_dodge())
 
 ui <- fluidPage(
+    theme = bs_theme(version = 4, bootswatch = "minty"),
     titlePanel("Top 100 Youtube Channels"),
     
     sidebarLayout(
         sidebarPanel(
-            h3("Select Input Data"),
+        conditionalPanel(
+            condition="input.tabset==1",
+            h3("Select Data"),
             radioButtons("radio_type",
                          h4("Type of dataset"),
                          choices = list("Entire" = 1, 
@@ -20,30 +31,89 @@ ui <- fluidPage(
                          selected = 1),
             sliderInput("slider_numberSubs",
                         h4("Number of Subscribers"),
-                        min = 0, max = max_subs, value = c(25, 75)),
+                        min = 0, max = max_subs, value = c(0, max_subs)),
         ),
+        conditionalPanel(
+            condition="input.tabset==2",
+            h3("Select Data"),
+            radioButtons("radio_type_2",
+                         h4("Type of dataset"),
+                         choices = list("Entire" = 1, 
+                                        "Only Youtube Channels" = 2, 
+                                        "Only Youtube Playlists" = 3),
+                         selected = 1),
+            h3("Select Statistics"),
+            checkboxGroupInput("stats_vars", "Variables:",
+                               c("statistics_viewCount","statistics_subscriberCount","statistics_videoCount"), selected = c("statistics_viewCount","statistics_subscriberCount","statistics_videoCount")),
+            checkboxGroupInput("stats_to_show","Statistics:",
+                               c("mean","median","variance"),
+                               selected=c("mean")),
+        ),
+        conditionalPanel(
+            condition="input.tabset==3",
+            h3("Select variables"),
+            radioButtons("radio_type_3",
+                         h4("Type of dataset"),
+                         choices = list("Entire" = 1, 
+                                        "Only Youtube Channels" = 2, 
+                                        "Only Youtube Playlists" = 3),
+                         selected = 1),
+            checkboxGroupInput("show_vars", "Variables to show:",
+                               names(data), selected = c("snippet_title","statistics_viewCount","statistics_subscriberCount","statistics_videoCount")),
+        )),
 
         mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("Distribution Plot", plotOutput("distribution_plot")),
-                        tabPanel("Summary", verbatimTextOutput("summary")),
-                        tabPanel("Table", tableOutput("table"))
-            )
+            tabsetPanel(id = "tabset",
+                        type = "tabs",
+                        tabPanel("Insights", 
+                                 plotOutput("insights_plot"),
+                                 value=4),
+                        tabPanel("Distribution Plot", 
+                                 plotOutput("distribution_plot"),
+                                 value=1),
+                        tabPanel("Summary Statistics",
+                                 DT::dataTableOutput("summary_stats"),
+                                 p("Confidence Intervals are computed using the bootstrapping technique."),
+                                 value=2),
+                        tabPanel("Dataset",
+                                 DT::dataTableOutput("data_table"),
+                                 value=3)
+        )
         )
     )
-    
 )
+
 
 server <- function(input, output) {
     data_to_show <- reactive({
-        print(input$radio_type)
         switch(input$radio_type, 
                "1"={to_show <- data},
                "2"={to_show <- data[data$youtube_playlist==0,]},
                "3"={to_show <- data[data$youtube_playlist==1,]})
-        print(to_show)
+        to_show <- to_show[to_show$statistics_subscriberCount>=input$slider_numberSubs[1] & to_show$statistics_subscriberCount<=input$slider_numberSubs[2],]
         return(to_show)
        })
+    
+    data_to_show_variables <- reactive({
+        switch(input$radio_type_3, 
+               "1"={to_show <- data},
+               "2"={to_show <- data[data$youtube_playlist==0,]},
+               "3"={to_show <- data[data$youtube_playlist==1,]})
+        to_show <- to_show[,input$show_vars]
+    })
+    
+    statistics_to_show <- reactive({
+        switch(input$radio_type_2, 
+               "1"={to_show <- data},
+               "2"={to_show <- data[data$youtube_playlist==0,]},
+               "3"={to_show <- data[data$youtube_playlist==1,]})
+        progress <- shiny::Progress$new()
+        on.exit(progress$close())
+        progress$set(message = "Making statistics", value = 0)
+        table_sum <- summary_stats_table(to_show,input$stats_vars,input$stats_to_show,progress)
+        return(table_sum)
+    })
+    
     output$distribution_plot<-renderPlot({
         dt <- data_to_show()
         ggplot(dt, aes(x=statistics_subscriberCount))+
@@ -53,6 +123,13 @@ server <- function(input, output) {
                        color="red", linetype="dashed", size=1)+
             geom_density(alpha=.2, fill="#FF6666")+
             labs(title="Subscribers", x="Subscriber count", y="Density")
+    })
+    output$data_table = DT::renderDataTable({
+        data_to_show_variables()
+    })
+    
+    output$summary_stats = DT::renderDataTable({
+        statistics_to_show()
     })
     
 }
---
+++ r_files/utils.R
@@ -0,0 +1,58 @@
+library(parallel)
+
+#preparing for code optimization/bootstrapping
+cores <- detectCores()
+
+which_stat <- function(x,stat){
+  switch(stat,
+         "median"={
+           return(median(x,na.rm=T))
+         },
+         "mean"={
+           return(mean(x,na.rm=T))
+         },
+         "variance"={
+           return(var(x,na.rm=T))
+         })
+}
+
+summary_stats <- function(stat, variable){
+  clusters <- makeCluster(cores-1)
+  clusterExport(clusters,c("stat","variable","which_stat"),envir=environment())
+  stat_boot <- parLapply(clusters, 1:5000, function(i) { 
+    x <- sample(variable, replace = TRUE)
+    which_stat(x,stat)
+  })
+  stopCluster(clusters)
+  return(stat_boot)
+}
+
+summary_stats_table <- function(data,variables,stats,progress){
+  print(variables)
+  print(stats)
+  rnames<-list()
+  df <- data.frame()
+  for(j in 1:length(stats)){
+    progress$inc(1/length(stats), detail = paste(j,"/",length(stats)))
+    ls <- list()
+    cf <- list()
+    for (i in 1:length(variables)){
+      s <- summary_stats(stats[j],data[,variables[i]])
+      which_s <- which_stat(data[,variables[i]],stats[j])
+      q <- paste(quantile(unlist(s), c(0.025, 0.975)), collapse=",")
+      ls[length(ls)+1]<-which_s
+      cf[length(cf)+1]<-paste0("[",q,"]",sep="")
+    }
+    df <- rbind(df,ls) 
+    rnames[length(rnames)+1]<-stats[j]
+    df <- rbind(df,cf) 
+    rnames[length(rnames)+1]<-paste(stats[j]," confidence int. (95%)", collapse = "")
+  }
+  colnames(df)<-variables
+  row.names(df)<-rnames
+  return(df)
+}
+
+
+
+
